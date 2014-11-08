ce4289d694bdd08e5be66f710e3951a8cd100379
Dan Bissonnet <dan@danisadesigner.com>
2014-11-08 13:00:02 +0000

    Enforce WP coding standards

--- index.php
+++ index.php
@@ -11,16 +11,13 @@ License: GPL2
 
 $dbisso_cfs_taxonomy_addon = new dbisso_cfs_taxonomy_addon();
 
-class dbisso_cfs_taxonomy_addon
-{
-    function __construct()
-    {
-        add_filter('cfs_field_types', array($this, 'cfs_field_types'));
-    }
+class dbisso_cfs_taxonomy_addon {
+	function __construct() {
+		add_filter( 'cfs_field_types', array( $this, 'cfs_field_types' ) );
+	}
 
-    function cfs_field_types($field_types)
-    {
-        $field_types['taxonomy'] = dirname(__FILE__) . '/taxonomy.php';
-        return $field_types;
-    }
+	function cfs_field_types( $field_types ) {
+		$field_types['taxonomy'] = dirname( __FILE__ ) . '/taxonomy.php';
+		return $field_types;
+	}
 }
